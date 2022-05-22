c63340b377f6552c7a30ffc62866dd5cf618016c
Ryen Beatty <ryenbeatty@deepspaenetwork.fritz.box>
2022-05-22 21:48:29 +0200

    prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -66,7 +66,8 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n 🪐 $(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
+
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
